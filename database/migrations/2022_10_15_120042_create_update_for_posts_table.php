37bf6a8789fb614920655949daa41ad06bf1645a
sharad9201 <sharad.rae19@gmail.com>
2022-10-15 17:50:24 +0545

    update posts table

---
+++ database/migrations/2022_10_15_120042_create_update_for_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('update_for_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('position')->nullable()->change();
+            $table->string('status')->change();
+            $table->string('description')->nullable()->change();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('update_for_posts');
+
+    }
+};
